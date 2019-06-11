b80c79fbb3a3e82929841ab3ae237307885c25de
Ethan Sagin <ethansagin@gmail.com>
2019-06-11 02:59:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,7 @@ class ApplicationController < Sinatra::Base
   end
   
   patch '/articles/:id' do
+    binding.pry
     @article = Article.find(params[:id])
     @article.update(title: params[:title], content: params[:content])
     redirect "/articles/#{article.id}"
